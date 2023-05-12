09950f39750299b2cf306dd707bdea68d0d41420
c0nstantin1990 <118696575+c0nstantin1990@users.noreply.github.com>
2023-05-12 10:10:11 -0400

    Determine past, present & future class

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,13 @@ for (var i = 9; i <= 11; i++) {
   // Create time-block element
   var timeBlock = $("div").attr("id", `hour-${i}`).addClass("row time-block");
 }
+
+//Determine past, present & future class
+var currentHour = new Date().getHours();
+if (i < currentHour) {
+  timeBlock.addClass("past");
+} else if (i === currentHour) {
+  timeBlock.addClass("present");
+} else {
+  timeBlock.addClass("future");
+}
