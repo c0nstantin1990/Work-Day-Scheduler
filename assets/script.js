7f500aa02391e9be03c3ca86a7c90247c34573d0
c0nstantin1990 <118696575+c0nstantin1990@users.noreply.github.com>
2023-05-12 13:23:22 -0400

    fixed issue with function

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,13 @@
-//Wait for the dom to load before running the script
 $(document).ready(function () {
-  //Dispay current day & time in header
+  // Display current day & time in header
   $("#currentDay").text(dayjs().format("dddd, MMMM D"));
 
-  // Looping through each time block and update its color base on the current time
+  // Looping through each time block and update its color based on the current time
   $(".time-block").each(function () {
-    // Creating time-block element
     var currentHour = dayjs().hour();
     var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
-    //Determine past, present & future class
+    // Determine past, present & future class
     if (blockHour < currentHour) {
       $(this).addClass("past");
     } else if (blockHour === currentHour) {
@@ -18,14 +16,16 @@ $(document).ready(function () {
       $(this).addClass("future");
     }
   });
-  //Save user input to local storage when button clicked
-  $("saveBtn").on("click", function () {
+
+  // Save user input to local storage when button clicked
+  $(".saveBtn").on("click", function () {
     var input = $(this).siblings(".description").val().trim();
     var time = $(this).parent().attr("id");
 
     localStorage.setItem(time, input);
   });
-  //Load saved input from local storage
+
+  // Load saved input from local storage
   $(".description").each(function () {
     var time = $(this).parent().attr("id");
     var input = localStorage.getItem(time);
